ce4e368c9fe5dea9e9163b34f27ca1218d8d5182
Juliana <ju.yamaguchi@gmail.com>
2026-06-16 23:58:01 +0200

    Refactor database connection setup to handle SQLite conditionally

--- api/src/database_client.js
+++ api/src/database_client.js
@@ -1,18 +1,23 @@
 import knex from "knex";
 
+const isSQLite = process.env.DB_CLIENT === "sqlite3";
+
 const connection = knex({
   client: process.env.DB_CLIENT,
-  connection: {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE_NAME,
-    filename: process.env.DB_SQLITE_FILENAME, // For SQLite
-    ssl:
-      process.env.DB_USE_SSL === "true" ? { rejectUnauthorized: false } : false,
-  },
-  useNullAsDefault: process.env.DB_USE_NULL_AS_DEFAULT === "true", // For SQLite
+  connection: isSQLite
+    ? { filename: process.env.DB_SQLITE_FILENAME }
+    : {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE_NAME,
+        ssl:
+          process.env.DB_USE_SSL === "true"
+            ? { rejectUnauthorized: false }
+            : false,
+      },
+  useNullAsDefault: isSQLite,
 });
 
 export default connection;
