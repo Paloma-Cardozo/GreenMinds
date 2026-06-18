ebc698a04e6ebad3f9144c8b0335e6c5f3226d8e
hiwot <99hiwot@gmail.com>
2026-06-18 19:30:20 +0200

    pg client added

--- api/src/database_client.js
+++ api/src/database_client.js
@@ -3,9 +3,10 @@ import knex from "knex";
 const isSQLite = process.env.DB_CLIENT === "sqlite3";
 
 const connection = knex({
-  client: process.env.DB_CLIENT,
-  connection: isSQLite
-    ? { filename: process.env.DB_SQLITE_FILENAME }
+  client: process.env.DB_CLIENT || "pg",
+  connection:
+    isSQLite ?
+      { filename: process.env.DB_SQLITE_FILENAME }
     : {
         host: process.env.DB_HOST,
         port: process.env.DB_PORT,
@@ -13,9 +14,9 @@ const connection = knex({
         password: process.env.DB_PASSWORD,
         database: process.env.DB_DATABASE_NAME,
         ssl:
-          process.env.DB_USE_SSL === "true"
-            ? { rejectUnauthorized: false }
-            : false,
+          process.env.DB_USE_SSL === "true" ?
+            { rejectUnauthorized: false }
+          : false,
       },
   useNullAsDefault: isSQLite,
 });
