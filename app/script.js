8672301d30bd66d325ccd19515ac15aa8b554065
Juliana <ju.yamaguchi@gmail.com>
2026-07-01 23:18:31 +0200

    fix: update API base URL to correct endpoint

--- app/script.js
+++ app/script.js
@@ -9,7 +9,7 @@ function selectPlant() {
 
 // const API_BASE_URL = "http://localhost:3001/api";
 
-const API_BASE_URL = "https://easybloom.onrender.com";
+const API_BASE_URL = "https://greenminds-fe0k.onrender.com/api";
 
 async function parseJsonResponse(response) {
   try {
